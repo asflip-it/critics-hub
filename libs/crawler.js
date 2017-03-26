65e0de049e6052dae86f15f8c68a5b723f5be1f4
Felipe Araujo Silva <asflip-it@protonmail.ch>
2017-03-25 21:38:37 -0300

    Visiting the first page, download its content and zip it

--- libs/crawler.js
+++ libs/crawler.js
@@ -1,1 +1,16 @@
-var request = require('request');
+// MODULES REQUIREMENTS
+const request = require('request');
+const zlib = require('zlib')
+const fs = require('fs')
+
+// SYSTEM CONSTANTS
+const gzip = zlib.createGzip()
+
+// Let's start by download a single page from gamespot
+
+request('https://www.gamespot.com/reviews/?page=1')
+  .on('error', err => {
+    console.log(`Error: ${err}`)
+  })
+  .pipe(gzip)
+  .pipe(fs.createWriteStream('data/page1.html.gz'))

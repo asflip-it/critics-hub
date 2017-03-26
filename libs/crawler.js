db117b293ec88cf944a1d7dae8347d1c3d1ac30a
Felipe Araujo Silva <asflip-it@protonmail.ch>
2017-03-26 11:04:48 -0300

    Transferring request code from the crawler.js to the Requester classes

--- libs/crawler.js
+++ libs/crawler.js
@@ -1,18 +1,7 @@
 "use strict"
 
-// MODULES REQUIREMENTS
-const request = require('request');
-const zlib = require('zlib')
-const fs = require('fs')
+const GamespotRequester = require('./requester.js').GamespotRequester
 
-for(let i = 1; i <= 5; i++) {
-  // create a new Gzip object for each page
-  let gzip = zlib.createGzip()
-  // Requesting a page and pipe the results for a gzip file
-  request(`https://www.gamespot.com/reviews/?page=${i}`)
-    .on('error', err => {
-      console.log(`Error: ${err}`)
-    })
-    .pipe(gzip)
-    .pipe(fs.createWriteStream(`data/page${i}.html.gz`))
-}
+let gs_req = new GamespotRequester('gsr-head-1', {})
+
+gs_req.get('https://www.gamespot.com/reviews/?page=10', '10')
--- libs/requester.js
+++ libs/requester.js
@@ -1,27 +1,38 @@
 "use strict"
 
+// REQUIRED MODULES
+const request = require('request');
+const zlib = require('zlib')
+const fs = require('fs')
+
 class BaseRequester {
 
-	constructor(id, options) {
-		this.id = id || '';
-		this.options = options;
-	}
+  constructor(id, options) {
+    this.id = id || '';
+    this.options = options || {};
+  }
 
-	get(url) {
-		throw new Error('Not yet implemented!')
-	}
+  get(url) {
+    throw new Error('Must be implemented on the super class')
+  }
 
 }
 
 class GamespotRequester extends BaseRequester {
 
-	constructor(id, options) {
-		super(id, options)
-	}
+  constructor(id, options) {
+    super(id, options)
+  }
 
-	get(url) {
-		throw new Error('Not yet implemented!')
-	}
+  get(url, id) {
+    let gzip = zlib.createGzip()
+    request(url)
+      .on('error', err => {
+        console.log(`Error: ${err}`)
+      })
+      .pipe(gzip)
+      .pipe(fs.createWriteStream(`data/page${id}.html.gz`))
+  }
 
 }
 

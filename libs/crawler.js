c1306070ff865783762e0e30bd54e0a91e35720c
Felipe Araujo Silva <asflip-it@protonmail.ch>
2017-03-26 00:37:32 -0300

    Iterating over a number of pages from the target url

--- libs/crawler.js
+++ libs/crawler.js
@@ -1,16 +1,18 @@
+"use strict"
+
 // MODULES REQUIREMENTS
 const request = require('request');
 const zlib = require('zlib')
 const fs = require('fs')
 
-// SYSTEM CONSTANTS
-const gzip = zlib.createGzip()
-
-// Let's start by download a single page from gamespot
-
-request('https://www.gamespot.com/reviews/?page=1')
-  .on('error', err => {
-    console.log(`Error: ${err}`)
-  })
-  .pipe(gzip)
-  .pipe(fs.createWriteStream('data/page1.html.gz'))
+for(let i = 1; i <= 5; i++) {
+  // create a new Gzip object for each page
+  let gzip = zlib.createGzip()
+  // Requesting a page and pipe the results for a gzip file
+  request(`https://www.gamespot.com/reviews/?page=${i}`)
+    .on('error', err => {
+      console.log(`Error: ${err}`)
+    })
+    .pipe(gzip)
+    .pipe(fs.createWriteStream(`data/page${i}.html.gz`))
+}
